a27db0c125c93be07954e09966b457681c6d53ae
DonPat99 <patricklee447536489912@gmail.com>
2026-05-15 05:00:58 +0000

    Update SurVis bibliography data

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,92 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "basak2019predicting": {
+        "author": "Basak, Suryoday and Kar, Saibal and Saha, Snehanshu and Khaidem, Luckyson and Dey, Sudeepa Roy",
+        "doi": "10.1016/j.najef.2018.06.013",
+        "journal": "The North American Journal of Economics and Finance",
+        "keywords": "traditional machine learning, random forest, XGBoost",
+        "title": "Predicting the Direction of Stock Market Prices Using Tree-Based Classifiers",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "year": "2019"
+    },
+    "chen2018which": {
+        "author": "Chen, Lin and Qiao, Zhilin and Wang, Minggang and Wang, Chao and Du, Ruijin and Stanley, Harry Eugene",
+        "doi": "10.1109/ACCESS.2018.2859809",
+        "journal": "IEEE Access",
+        "keywords": "comparative machine learning, deep learning, Chinese stock market",
+        "title": "Which Artificial Intelligence Algorithm Better Predicts the Chinese Stock Market?",
+        "type": "article",
+        "year": "2018"
+    },
+    "chen2024deep": {
+        "author": "Chen, Y. and others",
+        "doi": "10.32604/cmes.2023.031388",
+        "journal": "Computer Modeling in Engineering \\& Sciences",
+        "keywords": "survey, deep learning, financial time series",
+        "title": "Deep Learning for Financial Time Series Prediction: A State-of-the-Art Review of Standalone and Hybrid Models",
+        "type": "article",
+        "year": "2024"
+    },
+    "he2023financial": {
+        "author": "He, Kaijian and Yang, Qian and Ji, Lei and Pan, Jingcheng and Zou, Yingchao",
+        "doi": "10.3390/math11041054",
+        "journal": "Mathematics",
+        "keywords": "deep learning, ensemble model, financial time series",
+        "title": "Financial Time Series Forecasting with the Deep Learning Ensemble Model",
+        "type": "article",
+        "year": "2023"
+    },
+    "khodaee2022forecasting": {
+        "author": "Khodaee, Pouya and Esfahanipour, Akbar and Mehtari Taheri, Hassan",
+        "doi": "10.1016/j.engappai.2022.105464",
+        "journal": "Engineering Applications of Artificial Intelligence",
+        "keywords": "hybrid deep learning, CNN, LSTM, ResNet",
+        "title": "Forecasting Turning Points in Stock Price by Applying a Novel Hybrid CNN-LSTM-ResNet Model Fed by 2D Segmented Images",
+        "type": "article",
+        "year": "2022"
+    },
+    "kumbure2022machine": {
+        "author": "Kumbure, Mahinda Mailagaha and others",
+        "doi": "10.1016/j.eswa.2022.116659",
+        "journal": "Expert Systems with Applications",
+        "keywords": "survey, machine learning, stock forecasting",
+        "title": "Machine Learning Techniques and Data for Stock Market Forecasting: A Literature Review",
+        "type": "article",
+        "year": "2022"
+    },
+    "li2022incorporating": {
+        "author": "Li, Yawei and Lv, Shuqi and Liu, Xinghua and Zhang, Qiuyue",
+        "doi": "10.1155/2022/7739087",
+        "journal": "Complexity",
+        "keywords": "Transformer, attention, stock movement prediction",
+        "title": "Incorporating Transformers and Attention Networks for Stock Movement Prediction",
+        "type": "article",
+        "year": "2022"
+    },
+    "nabipour2020deep": {
+        "author": "Nabipour, Mojtaba and Nayyeri, Pooyan and Jabani, Hamed and Mosavi, Amir and Salwana, Ehsan and Shahab, Shamshirband",
+        "doi": "10.3390/e22080840",
+        "journal": "Entropy",
+        "keywords": "deep learning, LSTM, stock market prediction",
+        "title": "Deep Learning for Stock Market Prediction",
+        "type": "article",
+        "year": "2020"
+    },
+    "xie2024deep": {
+        "author": "Xie, Li and Chen, Zhengming and Yu, Sheng",
+        "doi": "10.3390/electronics13214225",
+        "journal": "Electronics",
+        "keywords": "DCT, CNN, Transformer, stock movement prediction",
+        "title": "Deep Convolutional Transformer Network for Stock Movement Prediction",
+        "type": "article",
+        "year": "2024"
+    },
+    "zhang2022transformer": {
+        "author": "Zhang, Qiuyue and Qin, Chao and Zhang, Yunfeng and Bao, Fangxun and Zhang, Caiming and Liu, Peide",
+        "doi": "10.1016/j.eswa.2022.117239",
+        "journal": "Expert Systems with Applications",
+        "keywords": "TEANet, Transformer, attention, stock movement prediction",
+        "title": "Transformer-Based Attention Network for Stock Movement Prediction",
+        "type": "article",
+        "year": "2022"
     }
 };
